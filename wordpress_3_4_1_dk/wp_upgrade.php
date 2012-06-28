71d2d0b895f0ff65f7ffe5843adf44f4105776ed
Sascha Bay <thecry@i-mscp.net>
2012-06-28 08:50:35 +0200

    Upgrade Joomla 2.5.6, Wordpress 3.4.1

---
+++ wordpress_3_4_1_dk/wp_upgrade.php
@@ -0,0 +1,11 @@
+<?php
+	$absolut_path = $argv[1];
+	define('WP_INSTALLING', true);
+	define('WP_MEMORY_LIMIT', '64M');
+	require_once($absolut_path.'wp-load.php');
+	timer_start();
+	require_once(ABSPATH .'wp-admin/includes/upgrade.php');
+	delete_site_transient('update_core');
+	wp_upgrade();
+	die( '0' );
+?>
