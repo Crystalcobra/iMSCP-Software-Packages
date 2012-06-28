0a0e060728874ec59cbe89f739c3e3719d204757
Sascha Bay <thecry@i-mscp.net>
2012-06-28 10:31:10 +0200

    Small Fix in wp_upgrade.php

--- wordpress_3_4_1_en/wp_upgrade.php
+++ wordpress_3_4_1_en/wp_upgrade.php
@@ -2,7 +2,7 @@
 	$absolut_path = $argv[1];
 	define('WP_INSTALLING', true);
 	define('WP_MEMORY_LIMIT', '64M');
-	require_once($absolut_path.'wp-load.php');
+	require_once($absolut_path.'/wp-load.php');
 	timer_start();
 	require_once(ABSPATH .'wp-admin/includes/upgrade.php');
 	delete_site_transient('update_core');
